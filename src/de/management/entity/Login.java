f8b12efa48c30516d5f561bd526e0512e211cc37
Kajus Lange <cloudleek@gmail.com>
2020-03-17 11:37:52 +0100

    Login Grundlagen

--- src/de/management/entity/Login.java
+++ src/de/management/entity/Login.java
@@ -1,5 +1,34 @@
 package de.management.entity;
 
-public class Login
-{
+public class Login {
+    // Instanzvariablen
+    private String username, passwort;
+
+    // Konstruktor
+    public Login(String username, String passwort) {
+        this.username = username;
+        this.passwort = passwort;
+    }
+
+    // Public Methoden
+    public boolean isLoginKorrekt(String[] loginInput) {
+        return loginInput[0].equals(username) && loginInput[1].equals(this.passwort);
+    }
+
+    // Getter und Setter
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
 }

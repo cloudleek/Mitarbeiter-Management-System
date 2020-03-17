4869af9472a0c61aa09cef29ef54e63bb3fc3ef6
Kajus Lange <cloudleek@gmail.com>
2020-03-17 11:40:17 +0100

    Bankverbindung Grundlagen

--- src/de/management/entity/Bankverbindung.java
+++ src/de/management/entity/Bankverbindung.java
@@ -1,5 +1,22 @@
 package de.management.entity;
 
 public class Bankverbindung {
-    private Object test; // GP
+    // Instanzvariablen
+    private String bankLeitZahl;
+    private String kontoNummer;
+
+    // Konstruktor
+    public Bankverbindung(String bankLeitZahl, String kontoNummer) {
+        this.bankLeitZahl = bankLeitZahl;
+        this.kontoNummer = kontoNummer;
+    }
+
+    // Getter und Setter
+    public String getBankLeitZahl() {
+        return bankLeitZahl;
+    }
+
+    public void setBankLeitZahl(String bankLeitZahl) {
+        this.bankLeitZahl = bankLeitZahl;
+    }
 }

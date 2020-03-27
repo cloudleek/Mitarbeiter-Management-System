495dba782fb87388d82a65e8fefff414c96a929b
Kajus Lange <cloudleek@gmail.com>
2020-03-27 17:53:33 +0100

    Datenbankzugriffe via JDBC

---
+++ src/de/management/database/DBZugriff.java
@@ -0,0 +1,46 @@
+package de.management.database;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Stellt eine Verbindung zur Datenbank zur Verfuegung.
+ */
+public class DBZugriff {
+    // Instanzvariablen
+    static Connection verbindung = null;
+    static final String dbUrl = "";
+
+    // Public Methoden
+
+    /**
+     * Returniert eine hergestellte Datenbankverbindung.
+     * @return Datenbankverbindung
+     */
+    public static Connection getVerbindung() {
+        try {
+            if(verbindung.isClosed() || verbindung.equals(null)) {
+                connectDatabase();
+                return verbindung;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return verbindung;
+    }
+
+    // Private Methoden
+
+    /**
+     * Stellt eine Datenbankverbindung her.
+     */
+    private static void connectDatabase() {
+        try {
+            verbindung = DriverManager.getConnection(dbUrl);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

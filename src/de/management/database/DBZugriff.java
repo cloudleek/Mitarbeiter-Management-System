69c08ddb2711f520c14809e1389c4f25ff3b755a
Kajus Lange <cloudleek@gmail.com>
2020-03-29 19:46:29 +0200

    Überprüft, ob eine eingegebene Kombination von Benutzerkennung und Passwort in der Datenbank hinterlegt ist.

--- src/de/management/database/DBZugriff.java
+++ src/de/management/database/DBZugriff.java
@@ -1,8 +1,6 @@
 package de.management.database;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Stellt eine Verbindung zur Datenbank zur Verfuegung.
@@ -29,6 +27,62 @@ public class DBZugriff {
         }
 
         return verbindung;
+    }
+
+    /**
+     * Gleicht die Eingabe der Benutzerkennung mit der Datenbank ab, ob die Benutzerkennung vorhanden ist.
+     * @param kennung Input des Benutzers.
+     * @return Wahr oder falsch Wert, ob die Benutzerkennung vorhanden ist.
+     */
+    public static boolean validateBenutzerkennung(String kennung) {
+        boolean validerInput = false;
+        // SQL Query
+        String query = "SELECT * FROM tbl_mitarbeiter WHERE name = ?";
+        // Datenbankzugriff
+        try {
+            PreparedStatement preparedStatement = getVerbindung().prepareStatement(query);
+            // Parameter setzen
+            preparedStatement.setString(1, kennung);
+            // Query ausfuehren
+            ResultSet resultSet = preparedStatement.executeQuery();
+            // Daten auswerten
+            validerInput = resultSet.next();
+            // Verbindung trennen
+            preparedStatement.close();
+            verbindung.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // Output
+        return validerInput;
+    }
+
+    /**
+     * Validiert den Login eines Benutzers in Abhaengigkeit von der Uebereinstimmung der Benutzerdaten mit der Datenbank.
+     * @return Wahr oder falsch Wert, ob der Login valide ist.
+     */
+    public static boolean validateLogin(String name, String passwort) {
+        boolean validerLogin = false;
+        // SQl Query
+        String query = "SELECT * FROM tbl_mitarbeiter WHERE name = ? AND passwort = ?";
+        // Datenbankzugriff
+        try {
+            PreparedStatement preparedStatement = getVerbindung().prepareStatement(query);
+            // Parameter setzen
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, passwort);
+            // Query ausfuehren
+            ResultSet resultSet = preparedStatement.executeQuery();
+            // Daten auswerten
+            validerLogin = resultSet.next();
+            // Verbindung trennen
+            preparedStatement.close();
+            verbindung.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // Output
+        return validerLogin;
     }
 
     // Private Methoden

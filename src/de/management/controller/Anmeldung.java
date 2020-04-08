f4dfe9182a35232d210179a3963590c87dfb3052
Kajus Lange <cloudleek@gmail.com>
2020-04-08 15:10:47 +0200

    Import Struktur, Kommentarposition, Szenenwechsel bei erfolgreichem Login

--- src/de/management/controller/Anmeldung.java
+++ src/de/management/controller/Anmeldung.java
@@ -1,15 +1,23 @@
 package de.management.controller;
-// JavaFX Imports
+
+// JFoenix Imports
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
-import de.management.database.DBZugriff;
+// JavaFX Imports
 import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+// Application Imports
+import de.management.database.DBZugriff;
 // Utility Imports
+import java.io.IOException;
 import java.net.URL;
-
 // SQL Imports
 import java.util.ResourceBundle;
 
@@ -18,19 +26,14 @@ import java.util.ResourceBundle;
  */
 public class Anmeldung implements Initializable {
     // Instanzvariablen
-    @FXML // FX:ID Referenzen fuer die benoetigten Elemente.
+    @FXML // FX:ID der Inputfelder
     private JFXTextField loginNameField, loginPasswordField;
+    @FXML // FX:ID der Loginschaltflaeche
     private JFXButton loginButton;
-
 
     // Public Methoden
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Wird der Button bataetigt, so wird der Input validiert.
-        loginButton.setOnAction(e -> {
-            inputValidieren();
-        });
-
         // Wird keine Benutzerkennung oder kein Passwort angegeben, wird der Button abgeschaltet.
         loginButton.disableProperty().bind(Bindings.createBooleanBinding(
                 () -> loginNameField.getText().isEmpty() || loginPasswordField.getText().isEmpty(),
@@ -40,15 +43,26 @@ public class Anmeldung implements Initializable {
 
     /**
      * Ueberprueft, ob die eingegebenen Logindaten in der Kombination in der Datenbank hinterlegt sind.
+     *
+     * @param event ActionEvent, wenn der Benutzer den Button anklickt.
      */
-    public void inputValidieren() {
+    public void inputValidieren(ActionEvent event) throws IOException {
         if(!DBZugriff.validateBenutzerkennung(loginNameField.getText())) {
             // TODO: 29/03/2020 Fehlermeldung: Falscher Benutzername
+            System.out.println("Falscher Benutzername!");
         } else {
             if(!DBZugriff.validateLogin(loginNameField.getText(), loginPasswordField.getText())) {
                 // TODO: 29/03/2020 Fehlermeldung: Falsches Passwort
-            } else {
-                // TODO: 29/03/2020 Benutzer weiterleiten: Abhaengig von der Position des Mitarbeiters
+                System.out.println("Falsches Passwort!");
+            } else { // Benutzer weiterleiten
+                // Szene des Dashboards laden
+                Parent dashboardViewParent = FXMLLoader.load(getClass().getResource("../view/views/mitarbeiterDashboard.fxml"));
+                Scene dashboardSzene = new Scene(dashboardViewParent);
+                // Stage Informationen der aktuellen Stage
+                Stage fenster = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                fenster.setScene(dashboardSzene);
+                fenster.centerOnScreen();
+                fenster.show();
             }
         }
     }

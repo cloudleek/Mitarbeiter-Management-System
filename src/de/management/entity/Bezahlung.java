7c8151bde763bd7e280fd95f1672c7078a6ce47e
Kajus Lange <cloudleek@gmail.com>
2020-04-09 16:07:56 +0200

    Bezahlungs-Entität

--- src/de/management/entity/Bezahlung.java
+++ src/de/management/entity/Bezahlung.java
@@ -1,4 +1,30 @@
 package de.management.entity;
 
 public class Bezahlung {
+    // Instanzvariablen
+    private String stufe;
+    private double betrag;
+
+    // Konstruktor
+    public Bezahlung(String stufe, double betrag) {
+        this.stufe = stufe;
+        this.betrag = betrag;
+    }
+
+    // Getter und Setter
+    public String getStufe() {
+        return stufe;
+    }
+
+    public void setStufe(String stufe) {
+        this.stufe = stufe;
+    }
+
+    public double getBetrag() {
+        return betrag;
+    }
+
+    public void setBetrag(double betrag) {
+        this.betrag = betrag;
+    }
 }

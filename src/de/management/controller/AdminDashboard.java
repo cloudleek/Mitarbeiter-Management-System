220b2fd603e441d94a5abe7d7bbf2a4620662580
Kajus Lange <cloudleek@gmail.com>
2020-05-17 17:04:27 +0200

    AdminDashboard: Template Implementation

---
+++ src/de/management/controller/AdminDashboard.java
@@ -0,0 +1,10 @@
+package de.management.controller;
+
+import de.management.entity.Mitarbeiter;
+
+public class AdminDashboard {
+
+    public void initialisiereDaten(Mitarbeiter mitarbeiter) {
+
+    }
+}
